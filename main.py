bb067fd289edd3f46035871add41ff977f75e629
Konrad Sniatala <k.sniatala@gmail.com>
2016-11-11 02:35:43 +0100

    contours with middle points

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2
 
+
 img = cv2.imread("img_ok/probe_cam1.JPG", cv2.IMREAD_COLOR)
 cv2.waitKey(0)
 
@@ -16,8 +17,35 @@ dilation = cv2.dilate(mask, kernel, iterations=0)
 kernel = np.ones((10,10), np.uint8)
 erosion = cv2.erode(dilation, kernel, iterations=3)
 im2, contours, hierarchy = cv2.findContours(erosion, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+#contours = contours[0] if imutils.is_cv2() else contours[1]
 img_copy = img
-cv2.drawContours(img_copy, contours, -1, (0,255,0), 3)
+# loop over the contours
+for c in contours:
+	# compute the center of the contour
+	M = cv2.moments(c)
+	if (M["m00"]==0):
+		M["m00"]=1
+	cX = int(M["m10"] / M["m00"])
+	cY = int(M["m01"] / M["m00"])
+ 
+	# draw the contour and center of the shape on the image
+	cv2.drawContours(img_copy, [c], -1, (0, 255, 0), 2)
+	cv2.circle(img_copy, (cX, cY), 7, (255, 255, 255), -1)
+	
+
+
+
+
+
+
+
+
+
+
+
+
+#cv2.drawContours(img_copy, contours, -1, (0,255,0), 3)
 # output = cv2.bitwise_and(hsv, hsv, mask=mask)
 
 cv2.namedWindow('img2', cv2.WINDOW_NORMAL)
